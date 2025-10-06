10654d49e6c5c3a72c33a3398ea945797365fa6a
m1lk1-w4y <cafzu541@gmail.com>
2025-10-06 13:03:34 -0500

    fix build errors 3

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -66,7 +66,8 @@ const HeroSection = () => {
                   Hire Me
                 </Link>
                 <Link
-                  href={r.cv}
+                  href="https://drive.google.com/file/d/1bG6jKHvA5_Eqfbya8JaA0N4IrwXjqesN/view?usp=drive_link"
+                  target="_blank"
                   className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
                 >
                   <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
