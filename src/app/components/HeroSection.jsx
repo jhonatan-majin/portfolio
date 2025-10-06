bf58add8c78d4953e6eb1156232498defd4e4c73
m1lk1-w4y <cafzu541@gmail.com>
2025-10-06 12:30:25 -0500

    fix errors

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -66,6 +66,7 @@ const HeroSection = () => {
                   Hire Me
                 </Link>
                 <Link
+                  key={r._id}
                   href={r.cv}
                   className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
                 >
